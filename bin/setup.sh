78abbf1da3993cbeb203baf0c1a16b2b57002ce7
macario <mail@makarius.me>
2014-02-17 10:15:40 -0600

    reordered setup functions, emmet vim plugin

--- bin/setup.sh
+++ bin/setup.sh
@@ -42,18 +42,6 @@ install_basics () {
     polipo quota-tools tor btrfs-progs redshift
 }
 
-# run as user
-setup_auto_login(){
-  sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
-  sudo sh -c "cat > /etc/systemd/system/getty@tty1.service.d/autologin.conf" <<EOF
-[Service]
-ExecStart=
-ExecStart=-/usr/bin/agetty --autologin $(whoami) --noclear %I 38400 linux
-Type=simple
-EOF
-
-}
-
 # run as root
 setup_postgresql(){
   systemd-tmpfiles --create postgresql.conf
@@ -79,137 +67,6 @@ setup_network(){
   systemctl enable dnsmasq
 
   sudo systemctl enable netctl-auto@wlp3s0.service
-}
-
-# run as user
-setup_remote_tunnels(){
-  # /usr/lib/systemd/systemd --user &
-  systemctl --user enable ssh-tunnel@maca-kujenga.co-9000-22.service
-  systemctl --user enable ssh-tunnel@maca-kujenga.co-3000-3000.service
-  systemctl --user enable ssh-tunnel@maca-kujenga.co-8080-80.service
-}
-
-# run as user
-setup_ssh_keys(){
-  echo "SSH Key setup"
-  ssh-keygen -t rsa -C "$(whoami)@$(cat /etc/hostname)"
-}
-
-# run as root
-# setup_aur_installs(){
-#   cd /tmp
-#   bash <(curl aur.sh) -si centerim5-git
-#   bash <(curl aur.sh) -si urxvt-perls
-#   bash <(curl aur.sh) -si xkbset
-#   bash <(curl aur.sh) -si powerdown-git
-#   bash <(curl aur.sh) -si pasystray-git
-#   bash <(curl aur.sh) -si pavumeter
-#   bash <(curl aur.sh) -si pulseaudio-ctl
-#   bash <(curl aur.sh) -si create_ap
-#   cd -
-# }
-
-# run as user
-setup_dotfiles(){
-  cd ~/dotfiles
-  git submodule init
-  git submodule update
-  git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-
-  ln -fs /tmp ~/Downloads
-  ln -fs $HOME/dotfiles/zshrc ~/.zshrc
-  ln -fs $HOME/dotfiles/zshenv ~/.zshenv
-  ln -fs $HOME/dotfiles/zprofile ~/.zprofile
-  ln -fs $HOME/dotfiles/vimrc ~/.vimrc
-  ln -fs $HOME/dotfiles/vim ~/.vim
-  ln -fs $HOME/dotfiles/conky ~/.conky
-  ln -fs $HOME/dotfiles/gitconfig ~/.gitconfig
-  ln -fs $HOME/dotfiles/tmux.conf ~/.tmux.conf
-  ln -fs $HOME/dotfiles/xinitrc ~/.xinitrc
-  ln -fs $HOME/dotfiles/Xresources ~/.Xresources
-  ln -fs $HOME/dotfiles/themes ~/.themes
-  ln -fs $HOME/dotfiles/ctags ~/.ctags
-  ln -fs $HOME/dotfiles/bin ~/bin
-
-  mkdir -p ~/.config/fontconfig/
-  ln -fs $HOME/dotfiles/awesome ~/.config/awesome
-  ln -fs $HOME/dotfiles/gtk-3.0 ~/.config/gtk-3.0
-  ln -sf $HOME/dotfiles/gtkrc-2.0 ~/.gtkrc-2.0
-  ln -fs $HOME/dotfiles/luakit ~/.config/luakit
-  ln -fs $HOME/dotfiles/fonts.conf ~/.config/fontconfig/fonts.conf
-  ln -fs $HOME/dotfiles/systemd ~/.config/systemd
-  ln -fs $HOME/dotfiles/redshift.conf ~/.config/redshift.conf
-}
-
-# run as user
-setup_development_environment(){
-  git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-  git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-  export PATH="$HOME/.rbenv/bin:$PATH"
-  eval "$(rbenv init -)"
-  rbenv install 2.0.0-p247
-  rbenv install 1.9.3-p448
-  rbenv global 2.0.0-p247
-  gem install bundler
-  gem install passenger
-  gem install yard
-  rbenv rehash
-  sudo passenger-install-nginx-module
-  sudo ln -fs $HOME/dotfiles/nginx.conf /opt/nginx/conf/nginx.conf
-  curl https://toolbelt.heroku.com/install.sh | sh
-
-  sudo sh -c "cat > /etc/systemd/system/nginx.service" <<EOF
-[Unit]
-Description=A high performance web server and a reverse proxy server
-After=syslog.target network.target
-
-[Service]
-Type=forking
-PIDFile=/run/nginx.pid
-ExecStartPre=/opt/nginx/sbin/nginx -t -q -g 'pid /run/nginx.pid; daemon on; master_process on;'
-ExecStart=/opt/nginx/sbin/nginx -g 'pid /run/nginx.pid; daemon on; master_process on;'
-ExecReload=/opt/nginx/sbin/nginx -g 'pid /run/nginx.pid; daemon on; master_process on;' -s reload
-ExecStop=/opt/nginx/sbin/nginx -g 'pid /run/nginx.pid;' -s quit
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-  sudo systemctl enable nginx
-
-  git clone https://github.com/creationix/nvm.git ~/.nvm
-  source ~/.nvm/nvm.sh
-  nvm install 0.10
-  nvm alias default 0.10
-}
-
-
-# run as user
-setup_documentation(){
-  mkdir -p ~/Documentation
-
-  mkdir -p ~/Documentation/jquery
-  cd ~/Documentation/jquery
-  curl -O http://jqapi.com/jqapi.zip
-  unzip jqapi.zip
-  rm jqapi.com
-
-  mkdir -p ~/Documentation/yard
-  mkdir -p ~/Documentation/yard/public
-  cat <<EOT > ~/Documentation/yard/config.ru
-require 'rubygems'
-require 'yard'
-
-libraries = {}
-gems = Gem.source_index.find_name('').each do |spec|
-  libraries[spec.name] ||= []
-  libraries[spec.name] << YARD::Server::LibraryVersion.new(spec.name, spec.version.to_s, nil, :gem)
-end
-
-run YARD::Server::RackAdapter.new libraries
-EOT
-
-  cd ~
 }
 
 # run as root
@@ -276,5 +133,148 @@ EOT
 
   systemctl enable tor
   systemctl enable polipo
+}
+
+# run as user
+setup_auto_login(){
+  sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
+  sudo sh -c "cat > /etc/systemd/system/getty@tty1.service.d/autologin.conf" <<EOF
+[Service]
+ExecStart=
+ExecStart=-/usr/bin/agetty --autologin $(whoami) --noclear %I 38400 linux
+Type=simple
+EOF
+
+}
+
+# run as user
+setup_dotfiles(){
+  cd ~/dotfiles
+  git submodule init
+  git submodule update
+  git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+  ln -fs /tmp ~/Downloads
+  ln -fs $HOME/dotfiles/zshrc ~/.zshrc
+  ln -fs $HOME/dotfiles/zshenv ~/.zshenv
+  ln -fs $HOME/dotfiles/zprofile ~/.zprofile
+  ln -fs $HOME/dotfiles/vimrc ~/.vimrc
+  ln -fs $HOME/dotfiles/vim ~/.vim
+  ln -fs $HOME/dotfiles/conky ~/.conky
+  ln -fs $HOME/dotfiles/gitconfig ~/.gitconfig
+  ln -fs $HOME/dotfiles/tmux.conf ~/.tmux.conf
+  ln -fs $HOME/dotfiles/xinitrc ~/.xinitrc
+  ln -fs $HOME/dotfiles/Xresources ~/.Xresources
+  ln -fs $HOME/dotfiles/themes ~/.themes
+  ln -fs $HOME/dotfiles/ctags ~/.ctags
+  ln -fs $HOME/dotfiles/bin ~/bin
+
+  mkdir -p ~/.config/fontconfig/
+  ln -fs $HOME/dotfiles/awesome ~/.config/awesome
+  ln -fs $HOME/dotfiles/gtk-3.0 ~/.config/gtk-3.0
+  ln -sf $HOME/dotfiles/gtkrc-2.0 ~/.gtkrc-2.0
+  ln -fs $HOME/dotfiles/luakit ~/.config/luakit
+  ln -fs $HOME/dotfiles/fonts.conf ~/.config/fontconfig/fonts.conf
+  ln -fs $HOME/dotfiles/systemd ~/.config/systemd
+  ln -fs $HOME/dotfiles/redshift.conf ~/.config/redshift.conf
+}
+
+# run as user
+setup_remote_tunnels(){
+   /usr/lib/systemd/systemd --user &
+  systemctl --user enable ssh-tunnel@maca-kujenga.co-9000-22.service
+  systemctl --user enable ssh-tunnel@maca-kujenga.co-3000-3000.service
+  systemctl --user enable ssh-tunnel@maca-kujenga.co-8080-80.service
+}
+
+# run as user
+setup_ssh_keys(){
+  echo "SSH Key setup"
+  ssh-keygen -t rsa -C "$(whoami)@$(cat /etc/hostname)"
+}
+
+# run as root
+# setup_aur_installs(){
+#   cd /tmp
+#   bash <(curl aur.sh) -si centerim5-git
+#   bash <(curl aur.sh) -si urxvt-perls
+#   bash <(curl aur.sh) -si xkbset
+#   bash <(curl aur.sh) -si powerdown-git
+#   bash <(curl aur.sh) -si pasystray-git
+#   bash <(curl aur.sh) -si pavumeter
+#   bash <(curl aur.sh) -si pulseaudio-ctl
+#   bash <(curl aur.sh) -si create_ap
+#   cd -
+# }
+
+# run as user
+setup_development_environment(){
+  git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+  git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init -)"
+  rbenv install 2.0.0-p247
+  rbenv install 1.9.3-p448
+  rbenv global 2.0.0-p247
+  gem install bundler
+  gem install passenger
+  gem install yard
+  rbenv rehash
+  sudo passenger-install-nginx-module
+  sudo ln -fs $HOME/dotfiles/nginx.conf /opt/nginx/conf/nginx.conf
+  curl https://toolbelt.heroku.com/install.sh | sh
+
+  sudo sh -c "cat > /etc/systemd/system/nginx.service" <<EOF
+[Unit]
+Description=A high performance web server and a reverse proxy server
+After=syslog.target network.target
+
+[Service]
+Type=forking
+PIDFile=/run/nginx.pid
+ExecStartPre=/opt/nginx/sbin/nginx -t -q -g 'pid /run/nginx.pid; daemon on; master_process on;'
+ExecStart=/opt/nginx/sbin/nginx -g 'pid /run/nginx.pid; daemon on; master_process on;'
+ExecReload=/opt/nginx/sbin/nginx -g 'pid /run/nginx.pid; daemon on; master_process on;' -s reload
+ExecStop=/opt/nginx/sbin/nginx -g 'pid /run/nginx.pid;' -s quit
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+  sudo systemctl enable nginx
+
+  git clone https://github.com/creationix/nvm.git ~/.nvm
+  source ~/.nvm/nvm.sh
+  nvm install 0.10
+  nvm alias default 0.10
+}
+
+
+# run as user
+setup_documentation(){
+  mkdir -p ~/Documentation
+
+  mkdir -p ~/Documentation/jquery
+  cd ~/Documentation/jquery
+  curl -O http://jqapi.com/jqapi.zip
+  unzip jqapi.zip
+  rm jqapi.zip
+
+  mkdir -p ~/Documentation/yard
+  mkdir -p ~/Documentation/yard/public
+  cat <<EOT > ~/Documentation/yard/config.ru
+require 'rubygems'
+require 'yard'
+
+libraries = {}
+gems = Gem.source_index.find_name('').each do |spec|
+  libraries[spec.name] ||= []
+  libraries[spec.name] << YARD::Server::LibraryVersion.new(spec.name, spec.version.to_s, nil, :gem)
+end
+
+run YARD::Server::RackAdapter.new libraries
+EOT
+
+  cd ~
 }
 
