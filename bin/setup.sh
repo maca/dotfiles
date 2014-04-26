1d68a9787850666f4e72f540a29e85bcb21ec994
macario <mail@makarius.me>
2014-04-26 15:20:46 -0500

    fixed gitconfig

--- bin/setup.sh
+++ bin/setup.sh
@@ -212,7 +212,7 @@ setup_development_environment(){
   git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
   export PATH="$HOME/.rbenv/bin:$PATH"
   eval "$(rbenv init -)"
-  rbenv install 2.0.0-p247
+  rbenv install 2.1.1
   rbenv install 1.9.3-p448
   rbenv global 2.0.0-p247
   gem install bundler
