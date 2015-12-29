201e56c4d67541848e2384689473e4a843803844
Macario <mail@makarius.me>
2015-12-28 18:16:05 -0600

    lock button for thinkpad

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -282,6 +282,7 @@ globalkeys = awful.util.table.join(
     awful.key({}, "XF86Display", function() awful.util.spawn_with_shell("/home/maca/bin/displays") end),
 
     -- Multimedia keys
+    awful.key({ }, "XF86Launch1",          function () awful.util.spawn("xtrlock") end),
     awful.key({ }, "XF86AudioMute",        function () awful.util.spawn("pulseaudio-ctl mute") end),
     awful.key({ }, "XF86AudioMicMute",     function () awful.util.spawn("pulseaudio-ctl mute-input") end),
     awful.key({ }, "XF86AudioRaiseVolume", function () awful.util.spawn("pulseaudio-ctl up") end),
--- bin/setup.sh
+++ bin/setup.sh
@@ -75,7 +75,7 @@ install_wm(){
     vlc gvim arandr
 
   cd /tmp
-  bash <(curl aur.sh) -si urxvt-font-size-git pulseaudio-ctl
+  bash <(curl aur.sh) -si urxvt-font-size-git pulseaudio-ctl xtrlock
   cd -
 }
 
