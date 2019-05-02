220c5fc316127fd44aa950c92b4bc358cf4c6324
Macario <maca@aelita.io>
2019-05-01 23:08:57 -0700

    thinkpad tearfree

--- bin/setup/thinkpad.sh
+++ bin/setup/thinkpad.sh
@@ -3,3 +3,13 @@
 aur -si tp-battery-mode
 sudo systemctl start tp-battery-mode
 sudo systemctl enable tp-battery-mode
+
+sudo pacman -Sy xf86-video-intel
+
+sudo sh -c "cat > /etc/X11/xorg.conf.d/20-intel.conf" <<EOF
+Section "Device"
+  Identifier  "Intel Graphics"
+  Driver      "intel"
+  Option      "TearFree" "true"
+EndSection
+EOF
