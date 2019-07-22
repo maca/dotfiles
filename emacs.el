22ff8f66062870ba30ed420bf70f79bb4d3e5373
Macario <maca@aelita.io>
2019-07-22 12:48:19 +0200

    fix emacs web-mode

--- emacs.el
+++ emacs.el
@@ -254,7 +254,7 @@
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
-  (add-to-list 'auto-mode-alist '("\\.html" . web-mode)
+  (add-to-list 'auto-mode-alist '("\\.html" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.eex" . web-mode))
 
   (use-package company-web
