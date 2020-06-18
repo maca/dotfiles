9c467b9d1770141ba6add9773fdc506f797194f9
Macario <maca@aelita.io>
2020-06-18 15:41:58 +0200

    fix ssh agent for emacs

--- bin/start-ssh-agent.sh
+++ bin/start-ssh-agent.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
-export SSH_AUTH_SOCK=/tmp/ssh-agent.socket
+export SSH_AUTH_SOCK=/run/user/1000/ssh-agent.socket
 ssh-add -l >/dev/null 2>&1
-if [ $? = 2 ]; then
+
+if [ $? -ne 0 ]; then
    rm -rf $SSH_AUTH_SOCK
-   eval $(ssh-agent -a $SSH_AUTH_SOCK)
+   eval $(ssh-agent -t 1h -a $SSH_AUTH_SOCK)
 fi
