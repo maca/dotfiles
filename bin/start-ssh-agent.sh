b6d04945dff320f4c60892c9f522837206bb0ee6
Macario <maca@aelita.io>
2020-08-03 05:50:16 +0200

    ssh agent init quiet

--- bin/start-ssh-agent.sh
+++ bin/start-ssh-agent.sh
@@ -5,5 +5,5 @@ ssh-add -l >/dev/null 2>&1
 
 if [ $? -ne 0 ]; then
    rm -rf $SSH_AUTH_SOCK
-   eval $(ssh-agent -t 1h -a $SSH_AUTH_SOCK)
+   eval $(ssh-agent -t 1h -a $SSH_AUTH_SOCK 2>/dev/null)
 fi
