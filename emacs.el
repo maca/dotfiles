135cc90aec3f880c7145088ce7e6f37fa3c56b59
Macario <maca@aelita.io>
2021-04-24 22:50:31 +0200

    emacs: autocomplete also uses current file

--- emacs.el
+++ emacs.el
@@ -270,8 +270,8 @@
   (setq company-idle-delay 0)
   (setq company-minimum-prefix-length 2)
   (setq company-show-numbers t)
+  (push '(company-etags company-keywords) company-backends)
 
-  (add-to-list 'company-backends 'company-etags)
   (define-key company-active-map [tab] 'company-complete-common-or-cycle)
 
   (add-hook 'after-init-hook 'global-company-mode))
