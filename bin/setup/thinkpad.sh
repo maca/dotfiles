585f9395fdedb194bdf6fc2e8335ccbd39c3c677
Macario <maca@aelita.io>
2021-12-08 01:56:51 +0100

    Fix screen tearing on setup

--- bin/setup/thinkpad.sh
+++ bin/setup/thinkpad.sh
@@ -7,68 +7,13 @@ sudo systemctl enable tp-battery-mode
 
 sudo sh -c "cat > /etc/X11/xorg.conf.d/20-intel.conf" <<EOF
 Section "Device"
-  Identifier "Intel Graphics"
-  Driver "modesetting"
+  Identifier  "Intel Graphics"
+  Driver      "i915"
+  Option      "TearFree" "true"
+  Option      "AccelMod" "uxa"
+  Option      "DRI"      "3"
 EndSection
 EOF
-
-
-# Backlight allowed for members of video group
-sudo sh -c "cat > /etc/udev/rules.d/backlight.rules" <<EOF
-ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="acpi_video0", RUN+="/bin/chgrp video /sys/class/backlight/%k/brightness"
-ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="acpi_video0", RUN+="/bin/chmod g+w /sys/class/backlight/%k/brightness"
-EOF
-
-
-sudo sh -c "cat > /etc/systemd/logind.conf" <<EOF
-[Login]
-#NAutoVTs=6
-#ReserveVT=6
-#KillUserProcesses=no
-#KillOnlyUsers=
-#KillExcludeUsers=root
-#InhibitDelayMaxSec=5
-#HandlePowerKey=poweroff
-#HandleSuspendKey=suspend
-#HandleHibernateKey=hibernate
-HandleLidSwitch=lock
-#HandleLidSwitchExternalPower=suspend
-#HandleLidSwitchDocked=ignore
-#PowerKeyIgnoreInhibited=no
-#SuspendKeyIgnoreInhibited=no
-#HibernateKeyIgnoreInhibited=no
-#LidSwitchIgnoreInhibited=yes
-#HoldoffTimeoutSec=30s
-#IdleAction=ignore
-#IdleActionSec=30min
-#RuntimeDirectorySize=10%
-#RemoveIPC=yes
-#InhibitorsMax=8192
-#SessionsMax=8192
-EOF
-
-
-### Fix opal sleep issues
-aur -si sedutil-sleep-git
-
-read -p "enter OPAL password: " password
-HASH=$(sudo sedutil-sleep --printPasswordHash $password /dev/nvme0n1)
-
-
-sudo sh -c "cat > /etc/systemd/system/sedutil-sleep.service" <<EOF
-[Unit]
-Description=sedutil-sleep
-
-[Service]
-Type=oneshot
-ExecStart=-+/usr/bin/sedutil-sleep -n -x --prepareForS3Sleep 0 $HASH /dev/nvme0n1
-RemainAfterExit=true
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-sudo systemctl enable sedutil-sleep.service
 
 
 # https://wiki.archlinux.org/index.php/laptop#Hibernate_on_low_battery_level
